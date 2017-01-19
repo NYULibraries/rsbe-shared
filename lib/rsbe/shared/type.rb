c2c2865e3591944fb59a43cb99f5e455e68e4b2d
jgpawletko <jgpawletko@gmail.com>
2017-01-18 21:25:53 -0500

    add Rsbe::Shared::Type#==

--- lib/rsbe/shared/type.rb
+++ lib/rsbe/shared/type.rb
@@ -8,6 +8,10 @@ module Rsbe
         @name = hash[:name]
         @code = hash[:code]
       end
+
+      def ==(other)
+        (name == other.name) && (code == other.code)
+      end
     end
   end
 end
--- spec/rsbe/shared/type_spec.rb
+++ spec/rsbe/shared/type_spec.rb
@@ -14,4 +14,16 @@ describe Rsbe::Shared::Type do
       expect(@sut.name).to eq('foo')
     end
   end
+
+  describe '#==' do
+    it 'should be true for two objects with the same attributes' do
+      sut1 = Rsbe::Shared::Type.new(name: 'foo', code: 'bar')
+      expect(@sut == sut1).to be true
+    end
+
+    it 'should be false for  two objects with different attributes' do
+      sut1 = Rsbe::Shared::Type.new(name: 'foo', code: 'baz')
+      expect(@sut == sut1).to be false
+    end
+  end
 end
